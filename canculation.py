408367eca3c098b6a2fb1efb2fd90e18e6538859
Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2024-09-05 17:43:37 +0500

    my job

---
+++ canculation.py
@@ -0,0 +1,54 @@
+from math import*
+def minus(a, b):
+    res = a-b
+    print("Ответ:",res)
+def plus(a, b):
+    res = a+b
+    print("Ответ:",res)
+def delenie(a, b):
+    res = a/b
+    print("Ответ:",res)
+def zarb(a, b):
+    res = a*b
+    print("Ответ:",res)
+def ispodkoren(a):
+    res = sqrt(a)
+    print(int(res))
+def foiz(a):
+    res = a/100
+    print("Ответ:",res)
+def stepen(a, b):
+    res = a**b
+    print("Ответ:",res)
+def dvichnakod(a):
+    if a == 0:
+        return "0"
+    res = ""
+    while a > 0:
+        res = str(a % 2) + res
+        a //= 2
+    print("Ответ:",res)
+while True:
+    n = int(input("1.Сложение\n2.Уменьшение\n3.Умножение\n4.Деление\n5.Корень\n6.Степень\n7.Двоичный кодирование числа\n8.Выход\nПожайлуста выбирите функцую: "))
+    if n == 1:
+        plus(int(input("Первое число: ")),int(input("Второе число: ")))
+    elif n == 2:
+        minus(int(input("Первое число: ")),int(input("Второе число: ")))
+    elif n == 3:
+        zarb(int(input("Первое число: ")),int(input("Второе число: ")))
+    elif n == 4:
+        delenie(int(input("Первое число: ")),int(input("Второе число: ")))
+    elif n == 5:
+        ispodkoren(int(input("Вводите число: ")))
+    elif n == 6:
+        stepen(int(input("Первое число: ")),int(input("Второе число: ")))
+    elif n == 7:
+        dvichnakod(int(input("Вводите число: ")))
+    elif n == 8:
+        print("Пока")
+        break
+    else:
+        print("Выбирете функциию из списка!!")
+        
+
+    
